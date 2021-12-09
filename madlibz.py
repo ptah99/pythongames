923868254745bcbc80cfdc9341db53d36ba29eb3
peter <85779012+ptah99@users.noreply.github.com>
2021-12-09 09:29:25 +0300

    Create madlibz.py

---
+++ madlibz.py
@@ -0,0 +1,12 @@
+# sttring concatenation (aka how to put strings together)
+youtuber = "peter!"
+print(f"subscribe to {youtuber}")
+# f string is the best
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous_person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because\
+ i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
