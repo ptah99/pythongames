7d95779a2d125b6febe99745e9a65538e391924a
peter <85779012+ptah99@users.noreply.github.com>
2021-12-09 09:32:36 +0300

    hangman project
    hangman game project

---
+++ hangman.py
@@ -0,0 +1,36 @@
+import random
+from hangman_list import words
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' 'in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+    # getting user input
+    while len(word_letters) > 0:
+
+        print('You have used these letters: ', ' '.join(used_letters))
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+           used_letters.add(user_letter)
+           if user_letter in word_letters:
+               word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
+
+user_input = input('Type something:')
+print(user_input)
